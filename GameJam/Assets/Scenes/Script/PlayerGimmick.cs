6cc813580fe75df33030dbec86284f9a8044a496
hakiyoshi <58187672+hakiyoshi@users.noreply.github.com>
2021-08-06 22:37:50 +0900

    ギミックに当たった際の動き修正

---
+++ GameJam/Assets/Scenes/Script/PlayerGimmick.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGimmick : MonoBehaviour
+{
+
+    //移動
+    private Vector2 EndPosi;//到達地点
+    private Vector2 StartPosi;//開始地点
+
+    [SerializeField] int MoveFlame;//移動フレーム
+    private int FlameCount = 0;
+
+    enum HIT : int
+    {
+        NONE = 0,
+        LAVA = 1,
+        NEEDLE = 2,
+        ICE = 3,
+    }
+    private int HitFlag = ((int)HIT.NONE);
+
+    //必要なもの
+    private Rigidbody2D rb;//物理
+    private PlayerLastField last;//最後の着地点
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        last = this.GetComponent<PlayerLastField>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (HitFlag != ((int)HIT.NONE))
+        {
+            rb.position = Move(StartPosi, EndPosi, (float)FlameCount / (float)MoveFlame);
+            rb.velocity = new Vector2(rb.velocity.x, 0.0f);
+
+            FlameCount++;
+
+            if (FlameCount == MoveFlame)
+            {
+                HitFlag = ((int)HIT.NONE);
+                FlameCount = 0;
+                rb.position = EndPosi;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Lava")//マグマヒット時
+        {
+            EndPosi = last.LastPosition;
+            StartPosi = rb.position;
+            last.LastPosiFlag = false;
+            HitFlag = ((int)HIT.LAVA);
+        }
+    }
+
+    //移動関数
+    //Timeは0.0f~1.0f
+    //Start始点
+    //End終点　
+    private Vector2 Move(Vector2 Start, Vector2 End, float Time)
+    {
+        Vector2 set = Start + ((End - Start) * Time);
+
+        return set;
+    }
+
+
+}
