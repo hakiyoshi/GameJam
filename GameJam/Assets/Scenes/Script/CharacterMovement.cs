fd44e86cd458efcd78adab3c88322dded603b159
RyotoKitauchi <ryot.kit.2103@icloud.com>
2021-08-06 18:00:24 +0900

    プレイヤー移動スクリプトにコメント付け

--- GameJam/Assets/Scenes/Script/CharacterMovement.cs
+++ GameJam/Assets/Scenes/Script/CharacterMovement.cs
@@ -16,7 +16,7 @@ public class CharacterMovement : MonoBehaviour
     LayerMask Gimmick_Layer;
 
     //ギミックの当たり判定用bool
-    bool bNeedle,bFire,bWater;
+    bool bNeedle,bLava,bIce;
 
     //移動方向判別用
     [SerializeField] float MaxSpeed;//最高速度
@@ -34,31 +34,40 @@ public class CharacterMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //ギミック耐性オブジェクト用配列
         Cols = new GameObject[3];
-
+        //右のギミック耐性オブジェクト用
         Cols[0] = transform.Find("Right").gameObject;
-
+        //上のギミック耐性オブジェクト用
         Cols[1] = transform.Find("Top").gameObject;
-        
+        //左のギミック耐性オブジェクト用
         Cols[2] = transform.Find("Left").gameObject;
 
+        //各オブジェクトを透明化(仮)
         for(int i = 0; i < Cols.Length; i++)
         {
             Cols[i].GetComponent<SpriteRenderer>().material.color = new Color(1, 1, 1, 0);
         }
 
+        //Rayで判定するレイヤー設定
         Gimmick_Layer = LayerMask.GetMask("Gimmick");
 
+        //物理演算コンポーネント取得
         rb = GetComponent<Rigidbody2D>();
 
-        bNeedle = bFire = bWater = false;
+        //各bool初期化
+        bNeedle = bLava = bIce = false;
 
+        //移動方向用数値初期化
         direction = 0f;
 
+        //角度初期化
         rotate = 0f;
 
+        //回転角度初期化
         now_Rotate = 0f;
 
+        //ジャンプカウント初期化
         jumpCount = 2;
     }
 
@@ -67,25 +76,29 @@ public class CharacterMovement : MonoBehaviour
     {
         Move();
 
-        
-
-        Collision2();
+        StartCoroutine("Collision2");
     }
 
     //キーボード入力等の処理
     void Move()
     {
+        //スペースキーを押したときのジャンプ処理
         if (Input.GetKeyDown(KeyCode.Space) && jumpCount != 0)
         {
+            //物理演算リセット
             rb.velocity = Vector2.zero;
 
+            //ジャンプカウント-1
             jumpCount -= 1;
 
+            //ジャンプ処理
             rb.AddForce(Vector2.up * 800);
 
+            //回転処理開始
             StartCoroutine("Rotarion");
         }
 
+        //キー入力での移動処理
         if (Input.GetKey(KeyCode.D))
             direction = MaxSpeed;
         else if (Input.GetKey(KeyCode.A))
@@ -94,59 +107,43 @@ public class CharacterMovement : MonoBehaviour
         //左右移動
         rb.position += new Vector2(direction, 0.0f);
 
-        this.transform.eulerAngles = new Vector3(0, 0, rotate);
+        //回転処理反映 
+        this.rb.transform.eulerAngles = new Vector3(0, 0, rotate);
 
         //疑似完成
-        direction *= 0.99f;
+        direction *= 0.8f;
     }
 
-    //ジャンプ時の回転の処理
+    //ジャンプ時の回転の処理(コルーチン)
     IEnumerator Rotarion()
     {
+        //回転速度インターバル初期化
         float time = 0.0f;
+
+        //回転角度計算
         now_Rotate -= 90;
 
+        //360度を超えたら0に戻す
         if (now_Rotate < -359)
                 now_Rotate = 0f;
 
+        //回転処理実行
         while (time <= 0.2f)
         {
+            //今の角度から90度回転
             rotate = Mathf.Lerp(now_Rotate + 90, now_Rotate, time / 0.1f);
+
+            //インターバル加算
             time += Time.deltaTime;
+
             yield return 0;
         }
     }
 
-    //ギミックに当たった時の処理(仮)
-    IEnumerator Colision()
+    //Ray当たり判定取得用(開発中)&(コルーチン)
+    IEnumerator Collision2()
     {
-        if(bNeedle || bFire || bWater)
-        {
-            int i = Mathf.Abs((int)(now_Rotate/ 90)) - 1;
-
-            if (0 <= i && i <= 2)
-            {
-                        Cols[i].GetComponent<Renderer>().material.color = setColor;
-            }
-            else
-            {
-                for (int j = 0; j < Cols.Length; j++)
-                        Cols[j].GetComponent<Renderer>().material.color = new Color(1, 1, 1, 0);
-            }
-
-            yield return new WaitForSeconds(0.01f);
-
-            this.transform.transform.position = new Vector2(-8.5f, -2.5f);
-            now_Rotate = rotate = 0f;
-            bNeedle = bFire = bWater = false;
-        }
-        
-    }
-
-    //Ray当たり判定取得用(開発中)
-    void Collision2()
-    {
-        //RaycastHit2D[,] hits = new RaycastHit2D[4, 6];
+        //RaycastHit2D[,] hits2 = new RaycastHit2D[4, 6];
 
         //for (int i = 0; i < 4; i++)
         //{
@@ -171,43 +168,74 @@ public class CharacterMovement : MonoBehaviour
 
         //    for (int j = 0; j < 6; j++)
         //    {
-        //        Vector3 now_Position = new Vector3(this.transform.position.x, this.transform.position.y + 2 * i);
+
+        //        Vector3 now_Position2 = new Vector3(this.transform.position.x, this.transform.position.y + 2 * i);
 
         //    }
         //}
 
-        RaycastHit2D[] hits = new RaycastHit2D[4];
-        Vector3 now_Position = new Vector3 (this.transform.position.x,this.transform.position.y);
-        Vector3[] end_Position = new Vector3[4];
-        end_Position[0] = now_Position + transform.right * 0.5f;
-        end_Position[1] = now_Position + transform.up * 0.5f;
-        end_Position[2] = now_Position - transform.right * 0.5f;
-        end_Position[3] = now_Position - transform.up * 0.5f;
+        //現在座標取得
+        Vector3 now_Position = new Vector3 (this.rb.transform.position.x,this.rb.transform.position.y);
 
+        //当たり判定の終点座標配列(上下左右)
+        Vector3[] end_Position = new Vector3[4];
+        //当たり判定の終点座標(右)
+        end_Position[0] = now_Position + rb.transform.right * 0.5f;
+        //当たり判定の終点座標(上)
+        end_Position[1] = now_Position + rb.transform.up * 0.5f;
+        //当たり判定の終点座標(左)
+        end_Position[2] = now_Position - rb.transform.right * 0.5f;
+        //当たり判定の終点座標(下)
+        end_Position[3] = now_Position - rb.transform.up * 0.5f;
+
+        //当たり判定用のRay配列(上下左右)
+        RaycastHit2D[] hits = new RaycastHit2D[4];
+        //当たり判定用のRay設定(右)
         hits[0] = Physics2D.Linecast(now_Position, end_Position[0], Gimmick_Layer);
+        //当たり判定用のRay設定(上)
         hits[1] = Physics2D.Linecast(now_Position, end_Position[1], Gimmick_Layer);
+        //当たり判定用のRay設定(左)
         hits[2] = Physics2D.Linecast(now_Position, end_Position[2], Gimmick_Layer);
+        //当たり判定用のRay設定(下)
         hits[3] = Physics2D.Linecast(now_Position, end_Position[3], Gimmick_Layer);
 
+        //当たり判定確認用ループ
         for (int i = 0; i < hits.Length; i++)
         {
+            //i番目のRayが当たったか
             if (hits[i])
             {
+                //デバッグでLineを見る用
                 Debug.DrawLine(now_Position, end_Position[i], Color.red);
+
+                //足元以外に当たったか
                 if (0 <= i && i <= 2)
+                {
+                    //当たった部分に色(耐性)を表示
                     Cols[i].GetComponent<Renderer>().material.color = setColor;
+                }
+                //足元に当たったら
                 else
                 {
+                    //全免疫削除処理
                     for (int j = 0; j < Cols.Length; j++)
+                        //全ての免疫を透明化
                         Cols[j].GetComponent<Renderer>().material.color = new Color(1, 1, 1, 0);
                 }
 
-                this.transform.transform.position = new Vector2(-8.5f, -2.5f);
+                //死んでからワープまでの待ち時間(仮)
+                yield return new WaitForSeconds(0.05f);
+
+                        //初期位置にワープ(場所は仮設定)
+                        this.rb.transform.transform.position = new Vector2(-8.5f, -2.5f);
+                        //各角度リセット
                         now_Rotate = rotate = 0f;
-                        bNeedle = bFire = bWater = false;
+                        //各boolリセット
+                        bNeedle = bLava = bIce = false;
             }
             else
             {
+                //デバッグでLineを見る用
                 Debug.DrawLine(now_Position, end_Position[i], Color.blue);
             }
         }
@@ -216,25 +244,30 @@ public class CharacterMovement : MonoBehaviour
     //当たり判定取得用(仮)
     void OnCollisionEnter2D(Collision2D collision)
     {
+        //地面との当たり判定
         if (collision.gameObject.CompareTag("Ground"))
         {
+            //ジャンプカウントリセット
             jumpCount = 2;
         }
+
+        //針との当たり判定
         if (collision.gameObject.CompareTag("Needle"))
         {
             bNeedle = true;
             setColor = Color.gray;
         }
+        //マグマとの当たり判定
         else if (collision.gameObject.CompareTag("Lava"))
         {
-            bFire = true;
+            bLava = true;
             setColor = Color.red;
         }
+        //氷との当たり判定
         else if (collision.gameObject.CompareTag("Ice"))
         {
-            bWater = true;
+            bIce = true;
             setColor = Color.cyan;
         }
     }
-
 }
