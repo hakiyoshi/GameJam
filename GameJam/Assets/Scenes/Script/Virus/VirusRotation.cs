65d48694f1eba81ac5b8bbc56aeeff5b9d02f9f6
hakiyoshi <58187672+hakiyoshi@users.noreply.github.com>
2021-08-08 02:16:08 +0900

    テクスチャの差し替え＆マグマの簡単なアニメーション

---
+++ GameJam/Assets/Scenes/Script/Virus/VirusRotation.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirusRotation : MonoBehaviour
+{
+    [SerializeField] float RotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        this.transform.rotation = this.transform.rotation * Quaternion.AngleAxis(Mathf.Deg2Rad * RotationSpeed, new Vector3(0.0f, 0.0f, 1.0f));
+    }
+}
