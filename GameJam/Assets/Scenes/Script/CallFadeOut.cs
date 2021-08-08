9d623d76aca3a33333e846bdc707a3970c7cf4a7
hakiyoshi <58187672+hakiyoshi@users.noreply.github.com>
2021-08-08 12:08:59 +0900

    Set

---
+++ GameJam/Assets/Scenes/Script/CallFadeOut.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CallFadeOut : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Fade.FadeOut("Test_Kitauchi");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
