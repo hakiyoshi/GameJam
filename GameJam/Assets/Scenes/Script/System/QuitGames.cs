c3d8d73eac6271f0076a1f6ddba438b52e07d326
hakiyoshi <58187672+hakiyoshi@users.noreply.github.com>
2021-08-30 23:43:05 +0900

    いろいろ修正

---
+++ GameJam/Assets/Scenes/Script/System/QuitGames.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGames : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_STANDALONE
+      UnityEngine.Application.Quit();
+#endif
+        }
+    }
+}
